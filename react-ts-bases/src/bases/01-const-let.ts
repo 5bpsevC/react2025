d39bd6d29fd1d7bfd23249f2dfc9716c7fe6bbca
brite gonzalez <127438633+brite6001@users.noreply.github.com>
2025-08-15 10:35:11 -0300

    Delete react-ts-bases directory

--- react-ts-bases/src/bases/01-const-let.ts
+++
@@ -1,9 +0,0 @@
-const firstName: string = "Fernando";
-let lastName = "Herrera";
-
-let diceNumber = 5;
-diceNumber = 3;
-
-const containsLetterH = lastName.includes("H");
-
-console.log({ containsLetterH, diceNumber, firstName, lastName });
