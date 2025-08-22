4ab2ef2e3d1d1339e4dbaa895c47a9c4ae6d58c7
brite6001 <brite6001@gmail.com>
2025-08-22 16:45:07 +0000

    suspense are added

--- 04-hooksApp/src/App.tsx
+++ 04-hooksApp/src/App.tsx
@@ -1,11 +1,23 @@
 // import { MemoCounter } from "./06-memos/MemoCounter";
-import { InstagramApp } from "./07-useOptimistic/InstagramApp";
+// import { InstagramApp } from "./07-useOptimistic/InstagramApp";
+import { Suspense } from "react";
+import { getUserAction } from "./08-use-suspense/api/get-user.action";
+import { ClientInformation } from "./08-use-suspense/ClientInformation";
+
+const id = crypto.randomUUID();
 
 function App() {
   return (
     <div>
-
-      <InstagramApp/>
+      <Suspense
+        fallback={
+          <div className="bg-gradient flex flex-col">
+            <h1 className="text-2xl">Cargando</h1>
+          </div>
+        }
+      >
+        <ClientInformation getUser={getUserAction(id)} />
+      </Suspense>
     </div>
   );
 }
